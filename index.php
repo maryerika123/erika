0a46ee8192fabba889b88dace5f1730eea3356c5
maryerika123 <53499740+maryerika123@users.noreply.github.com>
2019-08-14 09:45:14 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<center>
+<h1>AtregenioF photo Gallery<h1>
+<h2>List of Photos<h2>
+<br>
+
+
+
+<a href="page2.html">Photo 1</a> 
+<br>
+<a href="page3.html">Photo 2</a>
+<br>
+<a href="page4.html">Photo 3</a> 
+<br>
+
+
+
+
+</center>
+
+</body>
+
+</html>
